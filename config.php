c96c712db22cf6df76fd16409ec84655530bb965
L00117391 <33071146+L00117391@users.noreply.github.com>
2017-11-22 20:29:19 +0000

    Add files via upload
    Config file to be used with javascript file, created with sublime text editor 3

---
+++ config.php
@@ -0,0 +1,13 @@
+<!-- <?php
+	define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');//Enter localhost password here
+   define('DB_DATABASE', '');//Enter database name here
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+
+//include("config.php");
+   session_start();
+//These are to be put into the serperate page
+ -->
+
